aa2b7153e74f99f0adbd9499b0a1e66735c4ade4
jonathan <toleto2013@gmail.com>
2020-10-04 12:27:57 -0400

    user directory

---
+++ src/components/TableRow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./TableRow.css"
+
+function TableRow(){
+    return(
+        <tr>
+            <td>Jonathan</td>
+            <td>Santos</td>
+            <td></td>
+            <td>Male</td>
+            <td></td>
+            <td></td>
+        </tr>
+    )
+}
+export default TableRow;
